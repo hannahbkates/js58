47cafa29192535dfa4713b4dd2d9d9f618b3f568
Hannah Kates <hbk254@nyu.edu>
2017-05-14 21:37:42 -0400

    hw1 submission

---
+++ js-homework/hw1.sh
@@ -0,0 +1,65 @@
+cd
+cd Sites/
+mkdir my-awesome-directory
+cd my-awesome-directory
+mkdir nested-directory
+cd nested-directory
+touch hello.txt
+
+cd
+cd Sites/
+touch terminals-are-fun.txt
+
+cd
+cd Sites/
+mkdir 1
+cd 1
+mkdir 2
+cd 2
+mkdir 3
+cd 3
+mkdir 4
+cd 4
+mkdir 5
+cd 5
+mkdir 6
+cd 6
+mkdir 7
+cd 7
+mkdir 8
+cd 8
+mkdir 9
+cd 9
+mkdir hello
+cd hello
+mkdir its
+cd its
+mkdir me
+cd me
+touch adele.jpg
+
+cd
+cd Sites/
+mkdir i
+cd i
+mkdir was
+cd was
+mkdir wondering
+cd wondering
+mkdir if
+cd if
+mkdir after
+cd after
+mkdir all
+cd all
+mkdir these
+cd these
+mkdir years
+cd years
+mkdir you\ would
+cd you\ would
+mkdir like
+cd like
+mkdir to
+cd to
+touch meet.jpg
