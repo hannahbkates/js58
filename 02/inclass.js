eebe4c055e9041d56005c867acd4189b80b2e3eb
Hannah Kates <hbk254@nyu.edu>
2017-05-15 21:28:54 -0400

    cleaning and class 2

---
+++ 02/inclass.js
@@ -0,0 +1,17 @@
+var age = 34;
+
+if (age < 16) {
+	console.log('go to school');
+} else if (age >= 16 && age < 18) {
+	console.log('drive');
+} else if (age > 18 && age < 21) {
+	console.log('vote');
+} else if (age > 21 && age < 25) {
+	console.log('drink');
+} else if (age > 25 && age < 35) {
+	console.log('rent a car');
+} else if (age > 35 && age < 62) {
+	console.log('run for prez');
+} else {
+	console.log('get social security benefits');
+};
---
+++ 02/loops.js
@@ -0,0 +1,10 @@
+var age = 0;
+
+while(age < 100) {
+	age++
+	console.log('Current Age: ', age);
+};
+
+for(var i = 10; i < 10; i++) {
+	console.log('Current number: ' + i)
+}
